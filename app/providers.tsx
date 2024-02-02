f6f75d5674c6f31a06030e172e979495bf859237
dagfedlu <dagmawifedlu09@gmail.com>
2024-02-02 12:36:26 +0300

    fix runtime issues with Chakra UI. add provider for main layout

---
+++ app/providers.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+// change theme
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      varaints: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+})
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+}
---
+++ components/playerLayout.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Box } from "@chakra-ui/layout"
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Layout - common - here
+      {children}
+    </Box>
+  )
+}
+
+export default PlayerLayout
